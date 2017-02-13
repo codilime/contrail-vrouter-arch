b3b08074b27e12331a8cf08bdcadb98d7527bc86
Dariusz Sosnowski <dariusz.sosnowski@codilime.com>
2017-02-13 12:16:18 +0100

    Removed tables used in base64 encoding

--- windows/vrouter_mod.c
+++ windows/vrouter_mod.c
@@ -22,48 +22,10 @@ NDIS_HANDLE SxNBLPool = NULL;
 */
 PNDIS_RW_LOCK_EX AsyncWorkRWLock = NULL;
 
-static char encoding_table[] = {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-    'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-    'w', 'x', 'y', 'z', '0', '1', '2', '3',
-    '4', '5', '6', '7', '8', '9', '+', '/' };
-static int mod_table[] = { 0, 2, 1 };
-
 static char hex_table[] = {
     '0', '1', '2', '3', '4', '5', '6', '7',
     '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
 };
-
-char *base64_encode(const unsigned char *data, size_t input_length) {
-    size_t output_length = 4 * ((input_length + 2) / 3);
-
-    char *encoded_data = ExAllocatePoolWithTag(NonPagedPoolNx, output_length + 1, SxExtAllocationTag);
-    if (encoded_data == NULL) return NULL;
-
-    for (int i = 0, j = 0; i < input_length;) {
-
-        UINT32 octet_a = i < input_length ? (unsigned char)data[i++] : 0;
-        UINT32 octet_b = i < input_length ? (unsigned char)data[i++] : 0;
-        UINT32 octet_c = i < input_length ? (unsigned char)data[i++] : 0;
-
-        UINT32 triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
-
-        encoded_data[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
-        encoded_data[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
-        encoded_data[j++] = encoding_table[(triple >> 1 * 6) & 0x3F];
-        encoded_data[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
-    }
-
-    for (int i = 0; i < mod_table[input_length % 3]; i++)
-        encoded_data[output_length - 1 - i] = '=';
-    encoded_data[output_length] = 0;
-
-    return encoded_data;
-}
 
 /*  Dumps packet contents to the debug buffer. Packet contents will be formatted in
     Wireshark friendly format.
