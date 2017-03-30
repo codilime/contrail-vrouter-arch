b6510fc86d949afa68ec0abddae1ffa29befe5d0
Dariusz Sosnowski <dariusz.sosnowski@codilime.com>
2017-03-30 15:22:38 +0200

    ksync: review fixes

--- utils/vr_win_util.c
+++ utils/vr_win_util.c
@@ -4,7 +4,11 @@
 
 #define ETHER_ADDR_LEN	   6
 
-#define KSYNC_PATH "\\\\.\\vrouterKsync"
+#define KSYNC_MAX_WRITE_COUNT 4096
+const WCHAR *KSYNC_PATH = L"\\\\.\\vrouterKsync";
+
+// Handle for name pipe used by Ksync
+HANDLE hPipe;
 
 // TODO: JW-120 - Refactoring of vr_win_utils.c
 struct ether_addr {
@@ -70,7 +74,6 @@ struct ether_addr *
     static struct ether_addr addr;
     return ether_aton_r(asc, &addr);
 }
-HANDLE hPipe;
 
 // TODO: JW-120 - Refactoring of vr_win_utils.cl
 /* It is only temporary mock*/
@@ -80,14 +83,27 @@ nl_client_datagram_recvmsg(struct nl_client *cl)
     return nl_client_stream_recvmsg(cl);
 }
 
-LPSTR
-get_read_file_error_message(DWORD message_id, LPTSTR *message)
+DWORD
+get_error_message(DWORD message_id, LPTSTR *message)
 {
     DWORD flags = FORMAT_MESSAGE_ALLOCATE_BUFFER |
         FORMAT_MESSAGE_FROM_SYSTEM |
         FORMAT_MESSAGE_IGNORE_INSERTS;
     DWORD lang_id = MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT);
-    return FormatMessage(flags, NULL, message_id, lang_id, message, 0, NULL);
+    return FormatMessage(flags, NULL, message_id, lang_id, (LPTSTR)message, 0, NULL);
+}
+
+void
+print_last_error_message()
+{
+    DWORD error = GetLastError();
+    LPTSTR message = NULL;
+    if (get_error_message(error, &message) != 0) {
+        printf("Error: %ws [%d]\r\n", message, error);
+        LocalFree(message);
+    } else {
+        printf("Error: [%d]\r\n", error);
+    }
 }
 
 int
@@ -103,14 +119,7 @@ nl_client_stream_recvmsg(struct nl_client *cl)
         if (ReadFile(hPipe, buffer, NL_MSG_DEFAULT_SIZE, &dwRead, NULL)) {
             memcpy(cl->cl_buf, buffer, dwRead);
         } else {
-            DWORD error = GetLastError();
-            LPTSTR message = NULL;
-            if (get_read_file_error_message(error, &message) != 0) {
-                printf("Error: %s [%d]\r\n", message, error);
-                LocalFree(message);
-            } else {
-                printf("Error: [%d]\r\n", error);
-            }
+            print_last_error_message();
             ExitProcess(EXIT_FAILURE);
         }
     }
@@ -121,16 +130,25 @@ nl_client_stream_recvmsg(struct nl_client *cl)
 int
 nl_sendmsg(struct nl_client *cl)
 {
-    DWORD dwWritten;
-    int status = 0;
+    DWORD dwWritten = 0;
+    int status = -1;
 
     DWORD access_flags = GENERIC_READ | GENERIC_WRITE;
     DWORD attrs = OPEN_EXISTING;
 
-    hPipe = CreateFile(TEXT(KSYNC_PATH), access_flags, 0, NULL, attrs, 0, NULL);
+    hPipe = CreateFile(KSYNC_PATH, access_flags, 0, NULL, attrs, 0, NULL);
     if (hPipe != INVALID_HANDLE_VALUE) {
-        status = WriteFile(hPipe, cl->cl_buf, 4096, &dwWritten, NULL);
+        status = WriteFile(hPipe, cl->cl_buf, KSYNC_MAX_WRITE_COUNT, &dwWritten, NULL);
+        if (status == 0) {
+            goto handle_error;
+        }
+    } else {
+        goto handle_error;
     }
 
     return status;
+
+handle_error:
+    print_last_error_message();
+    return -1;
 }
