5fdbe5fe44153cde1f33d79162e7584275c04280
Mariusz Masztalerczuk <mmasztalerczuk@gmail.com>
2017-04-06 12:23:33 +0200

    JW-339 Clean up of vr_utils_temp.h (#59)
    * JW-339 Clean up of vr_utils_temp.h

--- include/vr_utils_temp.h
+++ include/vr_utils_temp.h
@@ -1,12 +1,10 @@
-#pragma once
+#ifndef __UTILS_TEMP__
+#define __UTILS_TEMP__
 
 #include "vr_os.h"
 #include <Ws2tcpip.h>
 #include <basetsd.h>
-
-typedef unsigned long   __kernel_size_t;
-
-#define GENL_ID_CTRL 0x10
+#include "netlink.h"
 
 enum {
     CTRL_CMD_UNSPEC,
@@ -22,80 +20,32 @@ enum {
     __CTRL_CMD_MAX,
 };
 
-
-#define IFNAMSIZ 16
-#define INET6_ADDRSTRLEN 46
-
-#define IFNAMSIZ 16
-#define AF_BRIDGE 7
-// netlink doesn't exist on windows platform
-#define AF_NETLINK 0
-#define NETLINK_GENERIC 0
-#define NLMSG_DONE  0x3
-
-
-// TODO: Remove unused defines and structures
-#define __attribute__(A) /* do nothing */
-
-#define NLM_F_REQUEST           1       /* It is request message.       */
-#define NLM_F_MULTI             2       /* Multipart message, terminated by NLMSG_DONE */
-#define NLM_F_ACK               4       /* Reply with ack, with zero or error code */
-#define NLM_F_ECHO              8       /* Echo this request            */
-#define NLM_F_DUMP_INTR         16      /* Dump was inconsistent due to sequence change */
-#define NLM_F_DUMP_FILTERED     32      /* Dump was filtered as requested */
-
-
-struct nlattr {
-	UINT16 nla_len;
-	UINT16 nla_type;
-};
-
-struct nlmsghdr {
-	UINT32           nlmsg_len;      /* Length of message including header */
-	UINT16           nlmsg_type;     /* Message content */
-	UINT16           nlmsg_flags;    /* Additional flags */
-	UINT32           nlmsg_seq;      /* Sequence number */
-	UINT32           nlmsg_pid;      /* Sending process port ID */
+enum {
+    CTRL_ATTR_UNSPEC,
+    CTRL_ATTR_FAMILY_ID,
+    CTRL_ATTR_FAMILY_NAME,
+    CTRL_ATTR_VERSION,
+    CTRL_ATTR_HDRSIZE,
+    CTRL_ATTR_MAXATTR,
+    CTRL_ATTR_OPS,
+    CTRL_ATTR_MCAST_GROUPS,
+    __CTRL_ATTR_MAX,
 };
 
 struct iov_iter {
-	int type;
-	size_t iov_offset;
-	size_t count;
-	union {
-		const struct iovec *iov;
-		const struct kvec *kvec;
-		const struct bio_vec *bvec;
-		struct pipe_inode_info *pipe;
-	};
-	union {
-		unsigned long nr_segs;
-		int idx;
-	};
+    int type;
+    size_t iov_offset;
+    size_t count;
+    union {
+        const struct iovec *iov;
+        const struct kvec *kvec;
+        const struct bio_vec *bvec;
+        struct pipe_inode_info *pipe;
+    };
+    union {
+        unsigned long nr_segs;
+        int idx;
+    };
 };
 
-struct genlmsghdr {
-	UINT8    cmd;
-	UINT8    version;
-	UINT16   reserved;
-};
-
-#define NLA_ALIGNTO 4
-#define NLMSG_ALIGNTO   4
-#define NLA_ALIGN(len) (((len)+NLA_ALIGNTO - 1) & ~(NLA_ALIGNTO - 1))
-#define NLA_HDRLEN ((int)NLA_ALIGN(sizeof(struct nlattr)))
-#define NLMSG_ALIGN(len) ( ((len)+NLMSG_ALIGNTO-1) & ~(NLMSG_ALIGNTO-1) )
-#define NLMSG_HDRLEN     ((int) NLMSG_ALIGN(sizeof(struct nlmsghdr)))
-#define GENL_HDRLEN     NLMSG_ALIGN(sizeof(struct genlmsghdr))
-
-enum {
-	CTRL_ATTR_UNSPEC,
-	CTRL_ATTR_FAMILY_ID,
-	CTRL_ATTR_FAMILY_NAME,
-	CTRL_ATTR_VERSION,
-	CTRL_ATTR_HDRSIZE,
-	CTRL_ATTR_MAXATTR,
-	CTRL_ATTR_OPS,
-	CTRL_ATTR_MCAST_GROUPS,
-	__CTRL_ATTR_MAX,
-};
+#endif /* __UTILS_TEMP__ */
