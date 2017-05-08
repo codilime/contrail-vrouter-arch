20b79fdbd30b6c36a029c194adbec438fdad3218
Michal Clapinski <michal.clapinski@codilime.com>
2017-05-08 17:13:24 +0200

    major review changes and one bugfix for windows branch

--- windows/installer/vrouterMSI/install_vRouter.ps1
+++
@@ -1,7 +0,0 @@
-param(
-    [Parameter()][string]$targetDir = "C:\Program Files\Juniper Networks\vRouter"
-)
-
-Invoke-Expression "Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root\ '$($targetDir)\vRouter.cer'"
-Invoke-Expression "Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher '$($targetDir)\vRouter.cer'"
-Invoke-Expression "netcfg -l '$($targetDir)\vRouter.inf' -c s -i vRouter"
